0a4e2dc18ef9ca9033a72beb16a5723020318182
smartXu <1527196854@qq.com>
2018-11-29 22:04:23 +0800

    修复两个bug tab 跳转

--- js/base.js
+++ js/base.js
@@ -287,6 +287,87 @@ function loadmore(element,url,userId,successFn) {
         })
     }
 };
+function loadmoreIndex(element,url,obj,successFn) {
+    $(element).on("scroll",function(){
+        //当前可视容器高度
+        var _elementHeight = $(element).outerHeight(),
+        //当前滚动区域高度
+            _elementChildHeight = $(element).children().outerHeight(),
+        //滚动条高度
+            _elementScroll = $(element).scrollTop();
+        //滚动区域 - 滚动条高度 > 可视高度  就是还可以滚动  表示没有滚动到底部  否则就到了底部
+        if(_elementChildHeight - _elementScroll - 10 > _elementHeight){
+            //console.log('没到底')
+        }else {
+            //console.log('到底了')
+            //当状态为0 的时候进行加载，防止重复加载
+            if(_loadState == 0){
+                //状态更新为1
+                _loadState = 1;
+                //增加页数
+                _loadIndex += 1;
+                //添加正在加载loadding
+                $(element).append('<p class="nowLoad">加载中...</p>');
+                //请求当前页数ajax
+                ajaxLoadNew(obj);
+            }
+        }
+    });
+    //ajax请求
+    function ajaxLoadNew(obj) {
+        //更新发向服务器的数据，添加页数key值
+       /* dataObj.page = page;
+        dataObj.size = 4;
+        dataObj.userId = userId;*/
+        obj.page=_loadIndex;
+        $.ajax({
+            url:baseUrl+url,
+            xhrFields:{
+                withCredentials:true
+            },
+            type:"post",
+            dataType:'json',
+            async:false,
+            data:obj,
+            success:function (data) {
+               // console.log(data);
+                //数据渲染  ajajx回调
+              successFn(data);
+                //当数据不为空的时候，更新状态
+                if(data.length > 0){
+                    //更新状态 为 0
+                    _loadState = 0;
+                    //删除正在加载loadding
+                    $('.nowLoad').remove();
+                   /* function hg(){
+                        $(".nowLoad").remove();
+                    }
+                    setTimeout(hg,1200);*/
+                }else {
+                    //当数据长度小于等于0的时候，代表没有数据了，更新状态为2
+                    _loadState = 2;
+                    //删除正在加载loadding
+                    $('.nowLoad').remove();
+                    //更换loadding为没有数据
+                    $(element).append('<p class="endLoad">没有数据了...</p>');
+                    function fg(){
+                        $(".endLoad").remove();
+                    }
+                    setTimeout(fg,3000);
+                }
+            },
+            error:function (err) {
+                //请求失败loadding
+                console.log(data);
+                sweetAlert(
+                   "sorry",
+                    data.msg,
+                    "error"
+                )
+            }
+        })
+    }
+};
 //上拉加载调用js
 /*loadmore('#wrapper','/store/tradeapi','post',{},function (data) {
  $.each(data.data.list,function (key,val) {
